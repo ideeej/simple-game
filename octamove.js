3579393684e9fad9c0969c85f03ade20d9556847
ideeej <ideeej@hotmail.com>
2020-12-22 12:06:59 -0300

    Initial Commit

---
+++ octamove.js
@@ -0,0 +1,22 @@
+
+
+
+/*
+
+const character = document.getElementById("character")
+const platform = document.getElementById("platform")
+
+
+YOU CAN USE EVENTS LIKE THIS:
+=====================================
+window.addEventListener("click", event => {
+    // the window was clicked
+})
+window.addEventListener("keyup", event => {
+    // the window received a keyup event
+
+})
+
+*/
+
+document.getElementById("charcter").addEventListener("click", event);
