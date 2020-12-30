f3a68e0358aedc6c2bb287cc55589d22ad3f5a95
agrawalpt <53204615+agrawalpt@users.noreply.github.com>
2020-12-30 18:22:09 +0530

    Update octamove.js

--- octamove.js
+++ octamove.js
@@ -1,22 +1,76 @@
 
 
 
-/*
+// images we need
+let octo_image
+let ground_image
+let tree_image
 
-const character = document.getElementById("character")
-const platform = document.getElementById("platform")
 
+// Octo position
+let x = 0
+let y = 0
 
-YOU CAN USE EVENTS LIKE THIS:
-=====================================
-window.addEventListener("click", event => {
-    // the window was clicked
-})
-window.addEventListener("keyup", event => {
-    // the window received a keyup event
+function preload() {
+     // Here you can load images before setup
+     
+     octo_image = loadImage("./assets/octocat.gif")
 
-})
+}
 
-*/
+function setup() {
+     // This function runs once at the start
 
-document.getElementById("charcter").addEventListener("click", event);
+     createCanvas(920,720);
+
+}
+
+function draw() {
+     // This function runs every frame
+
+     // Fill the canvas with (51, 51, 51)
+     background(51)
+     
+     update_coordinates()
+
+     image(octo_image, x, y);
+     
+}
+
+function update_coordinates() {
+     // References for learning p5.js
+     //
+     // P5 reference
+     // https://p5js.org/reference/#/p5/
+     //
+     // Daniel shiffman <3
+     // https://www.youtube.com/user/shiffman
+     
+     const speed = 10
+     
+     const w_down = keyIsDown("W".charCodeAt(0)) // 85 
+     const a_down = keyIsDown("A".charCodeAt(0)) // 65 
+     const s_down = keyIsDown("S".charCodeAt(0)) // 83
+     const d_down = keyIsDown("D".charCodeAt(0)) // 63
+
+     const up_arrow_down = keyIsDown(UP_ARROW)
+     const left_arrow_down = keyIsDown(LEFT_ARROW)
+     const down_arrow_down = keyIsDown(DOWN_ARROW)
+     const right_arrow_down = keyIsDown(RIGHT_ARROW)
+     
+     if(w_down || up_arrow_down) {
+          y += -speed
+     }
+
+     if (a_down || left_arrow_down) {
+          x += -speed
+     }
+     if(s_down || down_arrow_down){
+          y += speed
+     }
+
+     if (d_down || right_arrow_down) {
+          x += speed
+     }
+}
+
